bcd7e2073ddbc67de38220c1d25218941d9cc507
Tomohiro Nishimura <tomohiro68@gmail.com>
2021-08-21 14:47:46 +0900

    feat: isu_condition structでもgenerated columnを持つようにする

--- go/main.go
+++ go/main.go
@@ -81,13 +81,16 @@ type GetIsuListResponse struct {
 }
 
 type IsuCondition struct {
-	ID         int       `db:"id"`
-	JIAIsuUUID string    `db:"jia_isu_uuid"`
-	Timestamp  time.Time `db:"timestamp"`
-	IsSitting  bool      `db:"is_sitting"`
-	Condition  string    `db:"condition"`
-	Message    string    `db:"message"`
-	CreatedAt  time.Time `db:"created_at"`
+	ID           int       `db:"id"`
+	JIAIsuUUID   string    `db:"jia_isu_uuid"`
+	Timestamp    time.Time `db:"timestamp"`
+	IsSitting    bool      `db:"is_sitting"`
+	Condition    string    `db:"condition"`
+	Message      string    `db:"message"`
+	CreatedAt    time.Time `db:"created_at"`
+	IsDirty      bool      `db:"is_dirty"`
+	IsOverweight bool      `db:"is_overweight"`
+	IsBroken     bool      `db:"is_broken"`
 }
 
 type MySQLConnectionEnv struct {
