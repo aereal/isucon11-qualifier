8cf3977d81a8fb23b1736ad031291b0b3dda6043
Tomohiro Nishimura <tomohiro68@gmail.com>
2021-08-21 15:36:06 +0900

    feta: ALterするように変更

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -23,10 +23,6 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  `is_dirty` TINYINT(1) AS (`condition` REGEXP 'is_dirty=true') STORED NOT NULL,
-  `is_overweight` TINYINT(1) AS (`condition` REGEXP 'is_overweight=true') STORED NOT NULL,
-  `is_broken` TINYINT(1) AS (`condition` REGEXP 'is_broken=true') STORED NOT NULL,
-  `condition_level` TINYINT(1) AS (`is_dirty`+`is_overweight`+`is_broken`) STORED NOT NULL,
   KEY `timestamp_desc_jia_isu_uuid_idx` (`timestamp` DESC, `jia_isu_uuid`),
   KEY `timestamp_asc_jia_isu_uuid_idx` (`timestamp`, `jia_isu_uuid`),
   PRIMARY KEY(`id`)
